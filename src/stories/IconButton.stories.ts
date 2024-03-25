55d5e8a20bb1230861e3916b525c9be60de83213
Vero Chan <crazyveronik@gmail.com>
2024-03-25 11:15:53 +0100

    [BOD-194] Create IconButton component

---
+++ src/stories/IconButton.stories.ts
@@ -0,0 +1,58 @@
+import { StoryObj } from '@storybook/react'
+import { IconButton } from '../atoms/IconButton'
+
+const meta = {
+  title: 'Design System/Atoms/IconButton',
+  component: IconButton,
+  tags: ['autodocs'],
+  argTypes: {
+    ariaLabel: {
+      description: 'Accessible name & description of the element',
+    },
+    variant: {
+      description: 'IconButton variant used from a list of values',
+    },
+    disabled: {
+      description: 'Is the button in disabled state?',
+    },
+    icon: {
+      description: 'Icon from a list of values',
+      control: { type: 'select' },
+    },
+    href: {
+      description:
+        'If a link is provided, the component will be rendered as NextLink, otherwise as button',
+    },
+  },
+}
+
+const figmaPrimaryDesign = {
+  design: {
+    type: 'figma',
+    url: 'https://www.figma.com/file/DN2ova21vWqCRvPspBXgI1/Design-System?type=design&node-id=1873-922&mode=dev',
+  },
+}
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Link: Story = {
+  args: {
+    icon: 'Edit',
+    variant: 'primary',
+    ariaLabel: 'Edit game',
+    href: 'link.com',
+    disabled: false,
+  },
+  parameters: figmaPrimaryDesign,
+}
+
+export const Primary: Story = {
+  args: {
+    icon: 'Delete',
+    variant: 'primary',
+    ariaLabel: 'Delete game',
+    disabled: false,
+  },
+  parameters: figmaPrimaryDesign,
+}
