9c86a746178b23f4f3d3f5cbfc5ab979f9110596
Verónica Valls <crazyveronik@gmail.com>
2024-04-22 07:18:08 +0200

    [BOD-234] Fix Type errors

--- tests/Badge.spec.tsx
+++ tests/Badge.spec.tsx
@@ -1,14 +1,29 @@
 import React from 'react'
 import { render } from '@testing-library/react'
-import { Badge } from '@/atoms/Badge'
+import { Badge, Variant } from '@/atoms/Badge'
 
 describe('Badge', () => {
-  const variants = ['info', 'success', 'warning', 'error']
+  const variants = [
+    'info',
+    'success',
+    'warning',
+    'error',
+    'info-outlined',
+    'succes-outlined',
+    'warning-outlined',
+    'error-outlined',
+  ]
   it.each(variants)('renders the %s variant with text', (variant) => {
     const text = `${variant} badge text`
+    const accessibilityLabel = `${variant} badge label`
 
     const { getByRole, getByText } = render(
-      <Badge id={`${variant}-badge`} variant={variant} text={text} />,
+      <Badge
+        id={`${variant}-badge`}
+        accessibilityLabel={accessibilityLabel}
+        text={text}
+        variant={variant as Variant}
+      />,
     )
     expect(getByText(text)).toBeInTheDocument()
     expect(getByRole('status')).toHaveClass(`badge ${variant}`)
