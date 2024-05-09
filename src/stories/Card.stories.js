a0260bd42384f500a4c3dae4428db40edc6ca422
Verónica Valls <crazyveronik@gmail.com>
2024-05-09 11:23:26 +0200

    [BOD-266] Fix storybook glitch where right border is not visible

--- src/stories/Card.stories.js
+++ src/stories/Card.stories.js
@@ -37,48 +37,50 @@ export default meta
 
 export const ProductCard = {
   render: () => (
-    <Card variant="primary">
-      <CardHeader
-        isBold
-        title="Metal Gear Solid 5: Ground Zeroes + The Phantom Pain"
-      />
-      <CardContent>
-        <div
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            color: '#727272FF',
-            fontSize: '0.875rem',
-            fontWeight: '400',
-          }}
-        >
-          <span>PlayStation 4</span>
-          <div>
-            <span
-              style={{
-                color: '#444',
-                fontSize: '1rem',
-                fontWeight: '700',
-              }}
-            >
-              6,99€&nbsp;
-            </span>
-            <span
-              style={{
-                color: '#444',
-                fontSize: '1rem',
-                fontWeight: '400',
-              }}
-            >
-              / unit
-            </span>
+    <div style={{width: '98%'}}>
+      <Card variant="primary">
+        <CardHeader
+          isBold
+          title="Metal Gear Solid 5: Ground Zeroes + The Phantom Pain"
+        />
+        <CardContent>
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              color: '#727272FF',
+              fontSize: '0.875rem',
+              fontWeight: '400',
+            }}
+          >
+            <span>PlayStation 4</span>
+            <div>
+              <span
+                style={{
+                  color: '#444',
+                  fontSize: '1rem',
+                  fontWeight: '700',
+                }}
+              >
+                6,99€&nbsp;
+              </span>
+              <span
+                style={{
+                  color: '#444',
+                  fontSize: '1rem',
+                  fontWeight: '400',
+                }}
+              >
+                / unit
+              </span>
+            </div>
           </div>
-        </div>
-      </CardContent>
-      <CardFooter>
-        <Button variant="primary-outlined" label="Buy" />
-      </CardFooter>
-    </Card>
+        </CardContent>
+        <CardFooter>
+          <Button variant="primary-outlined" label="Buy" />
+        </CardFooter>
+      </Card>
+    </div>
   ),
 }
 
@@ -380,46 +382,50 @@ export const ProductCardsGroup = {
 
 export const Primary = {
   render: () => (
-    <Card variant="primary">
-      <CardHeader title="Tekken 8">
-        <IconButton
-          icon="Edit"
-          accessibilityLabel="Edit game"
-          href="link.com"
-        />
-        <IconButton
-          icon="Export"
-          accessibilityLabel="Export game"
-          href="link.com"
-        />
-        <IconButton
-          icon="Delete"
-          accessibilityLabel="Delete game"
-          href="link.com"
-        />
-      </CardHeader>
-      <CardContent>
-        <p>TEKKEN 8 will feature exciting new gameplay focused on “Aggressive” tactics. Retaining TEKKEN's unique fighting game identity, the game will provide both players and spectators with the series' most thrilling experience yet with visceral screen-jarring attacks and environments that are both dynamic and destructible.</p>
-      </CardContent>
-      <CardFooter>
-        <Button variant="primary-outlined" label="Add to wishlist" />
-      </CardFooter>
-    </Card>
+    <div style={{width: '98%'}}>
+      <Card variant="primary">
+        <CardHeader title="Tekken 8">
+          <IconButton
+            icon="Edit"
+            accessibilityLabel="Edit game"
+            href="link.com"
+          />
+          <IconButton
+            icon="Export"
+            accessibilityLabel="Export game"
+            href="link.com"
+          />
+          <IconButton
+            icon="Delete"
+            accessibilityLabel="Delete game"
+            href="link.com"
+          />
+        </CardHeader>
+        <CardContent>
+          <p>TEKKEN 8 will feature exciting new gameplay focused on “Aggressive” tactics. Retaining TEKKEN's unique fighting game identity, the game will provide both players and spectators with the series' most thrilling experience yet with visceral screen-jarring attacks and environments that are both dynamic and destructible.</p>
+        </CardContent>
+        <CardFooter>
+          <Button variant="primary-outlined" label="Add to wishlist" />
+        </CardFooter>
+      </Card>
+    </div>
   ),
 }
 
 export const Disabled = {
   render: () => (
-    <Card isDisabled={true} variant="primary">
-      <CardHeader title="Tekken 8">
-        <IconButton disabled icon="Delete" accessibilityLabel="Delete game" />
-      </CardHeader>
-      <CardContent>
-        <p>TEKKEN 8 will feature exciting new gameplay focused on “Aggressive” tactics. Retaining TEKKEN's unique fighting game identity, the game will provide both players and spectators with the series' most thrilling experience yet with visceral screen-jarring attacks and environments that are both dynamic and destructible.</p>
-      </CardContent>
-      <CardFooter>
-        <Button variant="primary-outlined" disabled label="Add to wishlist" />
-      </CardFooter>
-    </Card>
+    <div style={{width: '98%'}}>
+      <Card isDisabled={true} variant="primary">
+        <CardHeader title="Tekken 8">
+          <IconButton disabled icon="Delete" accessibilityLabel="Delete game" />
+        </CardHeader>
+        <CardContent>
+          <p>TEKKEN 8 will feature exciting new gameplay focused on “Aggressive” tactics. Retaining TEKKEN's unique fighting game identity, the game will provide both players and spectators with the series' most thrilling experience yet with visceral screen-jarring attacks and environments that are both dynamic and destructible.</p>
+        </CardContent>
+        <CardFooter>
+          <Button variant="primary-outlined" disabled label="Add to wishlist" />
+        </CardFooter>
+      </Card>
+    </div>
   ),
 }
