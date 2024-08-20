972d9fd07365add9359f729ff3a6087fdac59eaa
Verónica Valls <crazyveronik@gmail.com>
2024-08-20 07:30:58 +0200

    [BOD-503] Add Checkbox tests

---
+++ tests/Checkbox.spec.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import { Checkbox } from '@/atoms/Checkbox'
+
+describe('Checkbox', () => {
+  const variants = ['primary']
+
+  it.each(variants)('renders with label and expected styles', (variant) => {
+    const { getByRole, getAllByRole, getByText } = render(
+      <Checkbox
+        accessibilityLabel="Marks if the user likes videogames"
+        id="checkbox-videogames-preference"
+        label="Do you like videogames?"
+        variant="primary"
+      />,
+    )
+    expect(getAllByRole('generic')[1]).toHaveClass(`checkbox-group ${variant}`)
+    expect(getByRole('checkbox')).toHaveClass(`checkbox ${variant}`)
+    expect(getByText(/Do you like videogames/i)).toBeInTheDocument()
+  })
+})
