b388cbf92f00cf30c6a1d391acc51edd686e4384
Verónica Valls <crazyveronik@gmail.com>
2024-11-14 12:23:11 +0100

    [BOD-615] Add required hint to Select (DS version is wip)

---
+++ src/utils/useOutsideClick.ts
@@ -0,0 +1,25 @@
+import type { RefObject } from 'react'
+import { useEffect } from 'react'
+
+function useOutsideClick(
+  ref: RefObject<HTMLElement | null>,
+  callback: () => void,
+) {
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      const target = event.target as Node
+      if (!target || !target.isConnected) return
+
+      const isOutside = ref.current && !ref.current.contains(target)
+      if (isOutside) {
+        callback()
+      }
+    }
+    document.addEventListener('mousedown', handleClickOutside, true)
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside, true)
+    }
+  }, [ref])
+}
+
+export { useOutsideClick }
