22a7cbc77b9b57bfaf22450f5bde1f5b67815702
Verónica Valls <crazyveronik@gmail.com>
2025-01-31 08:37:45 +0100

    [BOD-794] Fix lint 💀

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -16,7 +16,7 @@ const config: StorybookConfig = {
     getAbsolutePath('@storybook/addon-essentials'),
     getAbsolutePath('@storybook/addon-interactions'),
     '@storybook/addon-a11y',
-    '@storybook/addon-designs'
+    '@storybook/addon-designs',
   ],
 
   framework: {
