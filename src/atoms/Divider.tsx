db6b97fc28b70581b9159bd8f660468ec487e9cc
LauraPorto <lauraporto596@gmail.com>
2025-02-12 10:38:25 +0100

    [BOD-816] Refactor divider

--- src/atoms/Divider.tsx
+++ src/atoms/Divider.tsx
@@ -1,30 +1,33 @@
 import './Divider.scss'
 import { classNames } from '../utils/classNames'
+import type { IconType } from './Icon'
 import { Icon } from './Icon'
 
-export interface DividerProps extends React.ComponentPropsWithoutRef<'hr'> {
+export interface DividerProps extends React.ComponentPropsWithoutRef<'div'> {
   title: string
   withIcon?: boolean
   variant?: string
+  iconName?: IconType
 }
 
 export function Divider({
   title,
   withIcon = false,
   variant = 'primary',
+  iconName = 'DeliveryNote',
   className,
 }: DividerProps) {
-  const cssClasses = classNames('divider-container', variant, className)
+  const cssClasses = classNames('divider', variant, className)
 
   return (
-    <div className={cssClasses}>
+    <div role="separator" className={cssClasses}>
       {withIcon ? (
-        <Icon name="DeliveryNote" size="3" />
+        <Icon name={iconName} size="3" />
       ) : (
-        <div className="short-line"></div>
+        <div className="short line"></div>
       )}
       <span>{title}</span>
-      <div className="line"></div>
+      <div className="long line"></div>
     </div>
   )
 }
--- tests/Divider.spec.tsx
+++ tests/Divider.spec.tsx
@@ -3,23 +3,29 @@ import { Divider } from '../src/atoms/Divider'
 
 describe('Divider', () => {
   it('renders', () => {
-    const { getAllByRole, getByText, debug } = render(
-      <Divider title="A title divider" withIcon={false} />,
+    const { getByRole, getAllByRole, getByText } = render(
+      <Divider
+        title="A title divider"
+        iconName="DeliveryNote"
+        withIcon={false}
+      />,
     )
-    debug()
-    expect(getAllByRole('generic')[1]).toHaveClass('divider-container primary')
-    expect(getAllByRole('generic')[2]).toHaveClass('short-line')
-    expect(getAllByRole('generic')[4]).toHaveClass('line')
+    expect(getByRole('separator')).toHaveClass('divider primary')
+    expect(getAllByRole('generic')[1]).toHaveClass('short line')
+    expect(getAllByRole('generic')[3]).toHaveClass('long line')
     expect(getByText('A title divider')).toBeInTheDocument()
   })
 
   it('renders with icon', () => {
-    const { debug, getByRole, getAllByRole, getByText } = render(
-      <Divider title="A title divider with icon" withIcon={true} />,
+    const { getByRole, getAllByRole, getByText } = render(
+      <Divider
+        title="A title divider with icon"
+        iconName="DeliveryNote"
+        withIcon={true}
+      />,
     )
-    debug()
     expect(getByRole('img')).toHaveClass(/icon/i)
-    expect(getAllByRole('generic')[3]).toHaveClass('line')
+    expect(getAllByRole('generic')[2]).toHaveClass('long line')
     expect(getByText('A title divider with icon')).toBeInTheDocument()
   })
 })
