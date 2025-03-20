cee10b3f96fa1d3fb46b985abfd78f2331baab95
LauraPorto <lauraporto596@gmail.com>
2025-03-20 18:15:59 +0100

    [BOD-868] QA refactor

--- tests/DateSinglePicker.spec.tsx
+++ tests/DateSinglePicker.spec.tsx
@@ -22,7 +22,7 @@ describe('DateSinglePicker', () => {
     expect(getByText('do')).toBeInTheDocument()
     expect(getByText('Fecha seleccionada: 23/1/2025')).toBeInTheDocument()
   })
-  it('renders with input that whos date picker value', () => {
+  it('renders with input that show date picker value', () => {
     const { getByRole } = render(
       <DateSinglePicker
         defaultValue={'2025-01-23'}
