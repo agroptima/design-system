81db30518907a5b2e4d515d710aca2b1727e2e00
Verónica Valls <crazyveronik@gmail.com>
2025-06-27 07:08:53 +0200

    [BOD-1152] Add fix because of accessibility issues

--- src/atoms/Checkbox.tsx
+++ src/atoms/Checkbox.tsx
@@ -55,7 +55,7 @@ export function Checkbox({
         disabled={disabled}
         className={classNames({ 'visually-hidden': hideLabel })}
       >
-        {children}
+        {children || accessibilityLabel}
       </Label>
     </div>
   )
