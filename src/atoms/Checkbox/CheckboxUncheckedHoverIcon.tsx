e470fc41a2d1a54bca6aa215caa8cd885ccc8099
Salva <saruba@gmail.com>
2025-11-14 18:01:03 +0100

    [BOD-1389] Checkbox

---
+++ src/atoms/Checkbox/CheckboxUncheckedHoverIcon.tsx
@@ -0,0 +1,18 @@
+export function CheckboxUncheckedHoverIcon(
+  props: React.SVGProps<SVGSVGElement>,
+) {
+  return (
+    <svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" {...props}>
+      <path
+        d="M0 2a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v16a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2Z"
+        fill="#fff"
+      />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M18 1H2a1 1 0 0 0-1 1v16a1 1 0 0 0 1 1h16a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1ZM2 0a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2Z"
+        fill="var(--primary-color-1000)"
+      />
+    </svg>
+  )
+}
