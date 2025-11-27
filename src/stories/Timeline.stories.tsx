ffd9ba8b4ba9df1563bc988e5c3524bb2f72c5d2
Verónica Valls <crazyveronik@gmail.com>
2025-11-27 10:20:08 +0100

    [BOD-1272] Add WIP timeline story

---
+++ src/stories/Timeline.stories.tsx
@@ -0,0 +1,128 @@
+import type { StoryObj } from '@storybook/nextjs'
+import React from 'react'
+import { CheckableTag, CheckableTagGroup } from '../atoms/CheckableTag'
+import { Collapsible } from '../atoms/Collapsible/Collapsible'
+import { DateRangePicker } from '../atoms/DatePicker/DateRangePicker'
+import { Milestone, Timeline } from '../atoms/Timeline'
+
+const figmaPrimaryDesign = {
+  design: {
+    type: 'figma',
+    url: 'https://www.figma.com/design/DN2ova21vWqCRvPspBXgI1/Design-System?node-id=4786-71&m=dev',
+  },
+}
+
+const meta = {
+  title: 'Design System/Atoms/Timeline',
+  component: Timeline,
+  tags: ['autodocs', 'Data display components'],
+  argTypes: {
+    id: {
+      description: 'Id for aria purposes',
+    },
+    variant: {
+      description: 'Component variant used',
+    },
+    title: {
+      description: 'Component title text',
+    },
+    buttons: {
+      description: 'Array of buttons to be shown on the footer',
+    },
+  },
+  parameters: {
+    docs: {
+      description: {
+        component:
+          '<h2>Usage guidelines</h2>' +
+          '<p>A Drawer is a panel that is typically overlaid on top of a page and slides in from the side. It contains a set of information or actions. Since the user can interact with the Drawer without leaving the current page, tasks can be achieved more efficiently within the same context.</p>',
+      },
+    },
+    figmaPrimaryDesign,
+  },
+}
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const TimelineDrawer: Story = {
+  render: () => (
+    <Timeline>
+      <Milestone
+        title="1985"
+        description="Nintendo
+NES launches with breakout hits like Super
+Mario Bros.
+Sega launches its Master System."
+      />
+      <Milestone
+        isSelected
+        title="1986"
+        description="Legend of Zelda comes out, spawning a long series of popular games."
+      />
+      <Milestone
+        title="1988"
+        description="Sega's 16-bit
+Mega Drive is introduced, becoming the company's most successful system."
+      />
+      <Milestone
+        title="1989"
+        description="Nintendo's handheld
+Game Boy hits the market."
+      />
+      <Milestone
+        title="1991"
+        description="Sega's hit Sonic the Hedgehog and the 16-bit
+Super Nintendo come out."
+      />
+      <Milestone
+        title="1994"
+        description="32-bit consoles like the Sega Saturn, Sony PlayStation, and NEC PC-FX launch."
+      />
+    </Timeline>
+  ),
+} as unknown as Story
+
+export const TimelineWithLinksDrawer: Story = {
+  render: () => (
+    <Timeline>
+      <Milestone
+        title="1985"
+        description="Nintendo
+NES launches with breakout hits like Super
+Mario Bros.
+Sega launches its Master System."
+        href="link.com"
+      />
+      <Milestone
+        isSelected
+        title="1986"
+        description="Legend of Zelda comes out, spawning a long series of popular games."
+        href="link.com"
+      />
+      <Milestone
+        title="1988"
+        description="Sega's 16-bit
+Mega Drive is introduced, becoming the company's most successful system."
+        href="link.com"
+      />
+      <Milestone
+        title="1989"
+        description="Nintendo's handheld
+Game Boy hits the market."
+        href="link.com"
+      />
+      <Milestone
+        title="1991"
+        description="Sega's hit Sonic the Hedgehog and the 16-bit
+Super Nintendo come out."
+        href="link.com"
+      />
+      <Milestone
+        title="1994"
+        description="32-bit consoles like the Sega Saturn, Sony PlayStation, and NEC PC-FX launch."
+        href="link.com"
+      />
+    </Timeline>
+  ),
+} as unknown as Story
