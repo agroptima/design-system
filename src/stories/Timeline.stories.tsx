83aff59094a15c1f44fef949ab8319797e47f275
Verónica Valls <crazyveronik@gmail.com>
2025-11-28 11:10:28 +0100

    [BOD-1272] Add Drawer structure to Timeline stories

--- src/stories/Timeline.stories.tsx
+++ src/stories/Timeline.stories.tsx
@@ -1,8 +1,6 @@
 import type { StoryObj } from '@storybook/nextjs'
 import React from 'react'
-import { CheckableTag, CheckableTagGroup } from '../atoms/CheckableTag'
-import { Collapsible } from '../atoms/Collapsible/Collapsible'
-import { DateRangePicker } from '../atoms/DatePicker/DateRangePicker'
+import { Drawer } from '../atoms/Drawer'
 import { Milestone, Timeline } from '../atoms/Timeline'
 import { LoadMoreMilestonesButton } from '../atoms/Timeline/LoadMoreMilestonesButton'
 
@@ -48,96 +46,136 @@ type Story = StoryObj<typeof meta>
 
 export const TimelineDrawer: Story = {
   render: () => (
-    <Timeline>
-      <Milestone
-        title="1985"
-        description="Nintendo
+    <Drawer
+      id="timeline-drawer"
+      title="1986"
+      buttons={[
+        {
+          label: 'Close',
+          variant: 'primary-outlined',
+          onClick: () => alert('click'),
+        },
+      ]}
+      onClose={() => alert('close')}
+    >
+      <div style={{ marginTop: '20px' }}>
+        <LoadMoreMilestonesButton
+          onClick={() => alert('more milestones')}
+          variant="primary"
+          title="View previous"
+          leftIcon="AngleUp"
+        />
+        <Timeline>
+          <Milestone
+            title="1985"
+            description="Nintendo
 NES launches with breakout hits like Super
 Mario Bros.
 Sega launches its Master System."
-      />
-      <Milestone
-        isSelected
-        title="1986"
-        description="Legend of Zelda comes out, spawning a long series of popular games."
-      />
-      <Milestone
-        title="1988"
-        description="Sega's 16-bit
+          />
+          <Milestone
+            isSelected
+            title="1986"
+            description="Legend of Zelda comes out, spawning a long series of popular games."
+          />
+          <Milestone
+            title="1988"
+            description="Sega's 16-bit
 Mega Drive is introduced, becoming the company's most successful system."
-      />
-      <Milestone
-        title="1989"
-        description="Nintendo's handheld
+          />
+          <Milestone
+            title="1989"
+            description="Nintendo's handheld
 Game Boy hits the market."
-      />
-      <Milestone
-        title="1991"
-        description="Sega's hit Sonic the Hedgehog and the 16-bit
+          />
+          <Milestone
+            title="1991"
+            description="Sega's hit Sonic the Hedgehog and the 16-bit
 Super Nintendo come out."
-      />
-      <Milestone
-        title="1994"
-        description="32-bit consoles like the Sega Saturn, Sony PlayStation, and NEC PC-FX launch."
-      />
-    </Timeline>
+          />
+          <Milestone
+            title="1994"
+            description="32-bit consoles like the Sega Saturn, Sony PlayStation, and NEC PC-FX launch."
+          />
+        </Timeline>
+        <LoadMoreMilestonesButton
+          onClick={() => alert('more milestones')}
+          variant="primary"
+          title="View next"
+          leftIcon="AngleDown"
+        />
+      </div>
+    </Drawer>
   ),
 } as unknown as Story
 
 export const TimelineWithLinksDrawer: Story = {
   render: () => (
-    <>
-      <LoadMoreMilestonesButton
-        onClick={() => alert('more milestones')}
-        variant="primary"
-        title="View previous"
-        leftIcon="AngleUp"
-      />
-      <Timeline>
-        <Milestone
-          title="1985"
-          description="Nintendo
+    <Drawer
+      id="timeline-drawer"
+      title="1986"
+      buttons={[
+        {
+          label: 'Close',
+          variant: 'primary-outlined',
+          onClick: () => alert('click'),
+        },
+      ]}
+      onClose={() => alert('close')}
+    >
+      <div style={{ marginTop: '20px' }}>
+        <LoadMoreMilestonesButton
+          onClick={() => alert('more milestones')}
+          variant="primary"
+          title="View previous"
+          leftIcon="AngleUp"
+        />
+        <Timeline>
+          <Milestone
+            title="1985"
+            description="Nintendo
 NES launches with breakout hits like Super
 Mario Bros.
 Sega launches its Master System."
-          href="link.com"
-        />
-        <Milestone
-          isSelected
-          title="1986"
-          description="Legend of Zelda comes out, spawning a long series of popular games."
-          href="link.com"
-        />
-        <Milestone
-          title="1988"
-          description="Sega's 16-bit
+            href="link.com"
+          />
+          <Milestone
+            isSelected
+            title="1986"
+            description="Legend of Zelda comes out, spawning a long series of popular games."
+            href="link.com"
+          />
+          <Milestone
+            title="1988"
+            description="Sega's 16-bit
 Mega Drive is introduced, becoming the company's most successful system."
-          href="link.com"
-        />
-        <Milestone
-          title="1989"
-          description="Nintendo's handheld
+            href="link.com"
+          />
+          <Milestone
+            title="1989"
+            description="Nintendo's handheld
 Game Boy hits the market."
-          href="link.com"
-        />
-        <Milestone
-          title="1991"
-          description="Sega's hit Sonic the Hedgehog and the 16-bit
+            href="link.com"
+          />
+          <Milestone
+            title="1991"
+            description="Sega's hit Sonic the Hedgehog and the 16-bit
 Super Nintendo come out."
-          href="link.com"
+            href="link.com"
+          />
+          <Milestone
+            title="1994"
+            description="32-bit consoles like the Sega Saturn, Sony PlayStation, and NEC PC-FX launch."
+            href="link.com"
+          />
+        </Timeline>
+        <LoadMoreMilestonesButton
+          onClick={() => alert('more milestones')}
+          variant="primary"
+          title="View next"
+          leftIcon="AngleDown"
         />
-        <Milestone
-          title="1994"
-          description="32-bit consoles like the Sega Saturn, Sony PlayStation, and NEC PC-FX launch."
-          href="link.com"
-        />
-      </Timeline>
-      <LoadMoreMilestonesButton
-        onClick={() => alert('more milestones')}
-        variant="primary"
-        title="View next"
-        leftIcon="AngleDown"
-      />
-    </>
+      </div>
+    </Drawer>
   ),
 } as unknown as Story
